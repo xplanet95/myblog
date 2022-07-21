2cab9b72b1e58a793baa14598cb7c8c90116f4f2
xplanet95 <vladis.box@yandex.ru>
2022-07-21 13:56:27 +0800

    index views and urls

--- myblog/myblog/settings.py
+++ myblog/myblog/settings.py
@@ -9,6 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
+dotenv.load_dotenv()
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 
