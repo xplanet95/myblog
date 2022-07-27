6bc40f3c9468938763d8223d8c43f6161f713645
xplanet95 <vladis.box@yandex.ru>
2022-07-27 14:47:47 +0800

    ckeditor

--- myblog/blog/admin.py
+++ myblog/blog/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 from .models import *
+from django import forms
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
+
+
+class PostAdminForm(forms.ModelForm):
+    content = forms.CharField(widget=CKEditorUploadingWidget())
+
+    class Meta:
+        model = Post
+        fields = '__all__'
 
 
 class CategoryAdmin(admin.ModelAdmin):
@@ -19,8 +29,7 @@ class TagAdmin(admin.ModelAdmin):
 
 
 class PostAdmin(admin.ModelAdmin):
-    # подключение класса через атрибут form, это для ckeditor
-    # form = NewsAdminForm
+    form = PostAdminForm
     prepopulated_fields = {'slug': ('title', )}
     list_display = ('pk', 'title', 'author', 'created_at', 'category', )
     list_display_links = ('pk', 'title')
@@ -29,6 +38,7 @@ class PostAdmin(admin.ModelAdmin):
     fields = ('title', 'slug', 'author', 'content', 'created_at', 'img', 'views', 'category', 'tags', )
     #  какие поля не редактируемые
     # readonly_fields = ('get_photo', 'cnt_views', 'created_at', 'updated_at')
+    readonly_fields = ('created_at', 'views', )
     save_on_top = True
 
 
--- myblog/myblog/urls.py
+++ myblog/myblog/urls.py
@@ -5,7 +5,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls'))
+    path('', include('blog.urls')),
+    path('ckeditor/', include('ckeditor_uploader.urls')),
 ]
 
 if settings.DEBUG:
