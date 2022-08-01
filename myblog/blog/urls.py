9466d8823594c0fa7b7c8409ee67b5a752fd9f38
xplanet95 <vladis.box@yandex.ru>
2022-08-01 15:59:43 +0800

    tru apply category

--- myblog/blog/urls.py
+++ myblog/blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('category/<str:slug>', views.get_category, name='get_category'),
 ]
--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -5,3 +5,7 @@ from django.http import HttpResponse
 def index(request):
     response = render(request, 'blog/index.html')
     return response
+
+
+def get_category(request):
+    return render(request, 'blog/index.html')
